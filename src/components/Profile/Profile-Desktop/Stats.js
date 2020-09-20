d8262b3e93e6346bd8fb9f2efc13ffba1466c875
Himanshu <himanshu@qilinlab.com>
2020-09-20 23:12:45 +0530

    Latest

--- src/components/Profile/Profile-Desktop/Stats.js
+++ src/components/Profile/Profile-Desktop/Stats.js
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import history from "../../../helpers/History"
 export default function Stats(props) {
     const {profile}=props
     return (
         <div className="stats-page">
             {profile!=null&&<>
-            <div className="stats-box">
+            <div className="stats-box" onClick={()=>history.push('/connection')} >
                 <div className="stat-box">
                 <h2>{profile.no_of_friend}</h2>
                 <p>Followers</p>
